d5509fae9756fefccf906bf43bee8c181146f727
Ayram Veloso <velosoayram@gmail.com>
2026-05-12 16:14:01 -0300

    update

--- prog_py/beecrowd/_2343.py
+++ prog_py/beecrowd/_2343.py
@@ -1,1 +1,17 @@
 # CAÇADORES DE MITOS.
+
+n = int(input())
+
+locais = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    
+    if [x, y] in locais:
+        print(1)
+        break
+
+    locais += [x, y]
+
+else:
+    print(0)
