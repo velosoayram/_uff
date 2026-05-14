b84fab2be08881aae5db3e6858c36c54a46629f1
Ayram Veloso <velosoayram@gmail.com>
2026-05-14 03:01:05 -0300

    update

---
+++ prog_py/beecrowd/1241.py
@@ -0,0 +1,7 @@
+# ENCAIXA OU NÃO II.
+
+n = int(input())
+for i in range(n):
+    a, b = map(str, input().split())
+    if b == a[(len(a)-len(b)):]: print('encaixa')
+    else: print('nao encaixa')
--- prog_py/beecrowd/2443.py
+++ prog_py/beecrowd/2443.py
@@ -1,43 +1,9 @@
-# FECHADURA. / wrong answer 75% / refazer.
+# SOMA DE FRAÇÕES.
 
-n, m = map(int, input().split())
-lista = list(map(int, input().split()))
-soma = 0
-for x in range(n):
-    if x == 0:
-        dist = lista[x] - m
-        soma += abs(dist)
-    else:
-        temp = dist
-        if lista[x] > m and lista[x-1] > m:
-            dist = max(lista[x], lista[x-1]) - m
-            if temp != dist:
-                val1 = min(dist, temp)
-                val2 = max(dist, temp)
-                soma += abs(val2) - abs(val1)
-                continue
-        elif lista[x] < m and lista[x-1] < m: 
-            dist = min(lista[x], lista[x-1]) - m
-            if temp != dist:
-                val1 = min(dist, temp)
-                val2 = max(dist, temp)
-                soma += abs(val1) - abs(val2)
-                continue
-        else:
-            dist = lista[x] - m
-        if temp == dist:
-            continue
-        else:
-            soma += abs(dist)
-print(soma)
-
-# FECHADURA 2.0
-
-n, m = map(int, input().split())
-lista = list(map(int, input().split()))
-soma = 0
-for i in range(n-1):
-    dif = m - lista[i]
-    soma += abs(dif)
-    lista[i+1] += dif
-print(soma)
+a, b, c, d = map(int, input().split())
+n1 = den = mdc = b * d
+n2 = num = (den // b * a) + (den // d * c)
+while num % den != 0:
+    num, den = den, num % den
+    mdc = den
+print(n2//mdc, n1//mdc)
