7b2cb7e460784975027bcdea036e4369c8be4bea
Ayram Veloso <velosoayram@gmail.com>
2026-05-15 14:23:42 -0300

    update

--- prog_py/beecrowd/2242.py
+++ prog_py/beecrowd/2242.py
@@ -1,2 +1,17 @@
+# HUAAUHAHHUAHAU. (teste)
+
+# c, vogais, risada = 0, ['a', 'e', 'i', 'o', 'u'], input()
+# for letra in risada:
+#     if letra in vogais: c += 1
+# if c >= (len(risada)//2): print('S')
+# else: print('N')
+
 # HUAAUHAHHUAHAU.
 
+vogais, ordem, risada = ['a', 'e', 'i', 'o', 'u'], [], input()
+for letra in risada:
+    if letra in vogais:
+        ordem.append(letra)
+ordem = ''.join(ordem)
+ordem_dec = ordem[::-1]
+print('S' if ordem == ordem_dec else 'N')
