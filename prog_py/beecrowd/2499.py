d995424f873564355a9d4178a5fe0d5d89b52071
Ayram Veloso <velosoayram@gmail.com>
2026-05-18 19:46:05 -0300

    update

--- prog_py/beecrowd/2499.py
+++ prog_py/beecrowd/2499.py
@@ -1,1 +1,12 @@
 # TRIÂNGULO INTERNO.
+
+while True:
+
+    s, n, m = map(int, input().split())
+    if s == n == m == 0:
+        break
+    c1, c2, c3 = map(int, input().split())
+    numerador = s * abs(c2 - c3) * c1
+    denominador = (m + 1) * (n + 1)
+    resultado = numerador // denominador
+    print(resultado)
