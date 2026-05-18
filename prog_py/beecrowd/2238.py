c73252f9687fda0ef3472bca637f01e913e3cd61
Ayram Veloso <velosoayram@gmail.com>
2026-05-18 14:03:23 -0300

    update

--- prog_py/beecrowd/2238.py
+++ prog_py/beecrowd/2238.py
@@ -1,4 +1,15 @@
 # DIVISORES.
 
 a, b, c, d = map(int, input().split())
-
+candidatos, flag = list(), False
+for i in range(1, int(c**(1/2)) + 1):
+    if c % i == 0:
+        candidatos.append(i)
+        if i != c // i:
+            candidatos.append(c // i)
+for i in sorted(candidatos):
+    if i % a == 0 and i % b != 0 and d % i != 0:
+        print(i)
+        flag = True
+        break
+if not flag: print(-1)
--- prog_py/beecrowd/2309.py
+++ prog_py/beecrowd/2309.py
@@ -1,1 +1,21 @@
 # TRUCO.
+
+n = int(input())
+seq = {4:1, 5:2, 6:3, 7:4, 11:6,
+       12:5, 13:7, 1:8, 2:9, 3:10}
+placar = [0, 0]
+for x in range(n):
+    adal = bern = 0     
+    a1, a2, a3, b1, b2, b3 = map(int, input().split())
+    if seq[a1] >= seq[b1]:
+        adal += 1
+    else: bern += 1
+    if seq[a2] >= seq[b2]:
+        adal += 1
+    else: bern += 1
+    if seq[a3] >= seq[b3]:
+        adal += 1
+    else: bern += 1
+    if adal > bern: placar[0] += 1
+    else: placar[1] += 1
+print(placar[0], placar[1])
