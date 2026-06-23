d9ab267a9a8a83c327034a6f412d71fb35d52971
Ayram Veloso <velosoayram@gmail.com>
2026-06-22 23:30:46 -0300

    update

--- prog_py/beecrowd/_2305.py
+++ prog_py/beecrowd/_2305.py
@@ -1,1 +1,55 @@
 # COLHEITA DE CAJU.
+
+def colheita(matriz, m, n):
+
+    tot_linhas = len(matriz)
+    tot_colunas = len(matriz[0])
+
+    # CRIAR SUMMED-AREA-TABLE.
+    sat = [[0] * (tot_colunas + 1) for l in range(tot_linhas + 1)]
+
+    # PREENCHER SUMMED-AREA-TABLE.
+    for l in range(1, tot_linhas + 1):
+        for c in range(1, tot_colunas + 1):
+
+            valor = matriz[l - 1][c - 1]
+            sat[l][c] = valor - sat[l - 1][c - 1] + sat[l - 1][c] + sat[l][c - 1]
+
+
+    # BUSCA PELO MAIOR.
+    maior_colheita = 0
+
+    for l in range():
+        for c in range():
+
+            None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    return print()
+
+
+l, c, m, n = map(int, input().split())
+matriz = []
+
+for linhas in range(l):
+    
+    matriz.append(list(map(int, input().split())))
+
+colheita(matriz, m, n)
---
+++ prog_py/conceitos/summedarea.py
@@ -0,0 +1,10 @@
+# SUMMED AREA TABLE.
+
+
+while True:
+    
+    linhas, colunas = map(int, input().split())
+
+    matriz = [[0] * colunas for l in range(linhas)]
+        
+    print(matriz)
