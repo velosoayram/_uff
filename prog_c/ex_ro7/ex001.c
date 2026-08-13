e08385f1dd778cbb73447edb5eaad4f899f6adda
Ayram Veloso <velosoayram@gmail.com>
2026-08-13 13:45:34 -0300

    update

---
+++ prog_c/ex_ro7/ex001.c
@@ -0,0 +1,37 @@
+/* Q1) Implemente um programa que, infinitamente, receba, como parâmetro de entrada, um número
+n e retorne os n primeiros números primos existentes. Seu programa para quando n for menor ou
+igual a zero. */
+
+#include <stdio.h> 
+
+int fprimo(int n) {
+
+    if (n < 2) return 0;
+    else {
+        int i;
+        for (i = 2; i * i <= n; i++) {
+            if (n % i == 0) return 0;
+        }
+    }
+    return 1;
+}
+
+
+int main(void) {
+    while(1) {
+        int c_ind = 2;
+        int c_tot = 0;
+        int num;
+        scanf("%d", &num);
+        if (num <= 0) break;
+        while(c_tot < num) {
+            if (fprimo(c_ind)) {
+                printf("%d ", c_ind);
+                c_tot++;
+            }
+            c_ind++;
+            if (c_tot == num) printf("\n");
+        }
+    }
+    return 0;
+}
---
+++ prog_c/ex_ro7/ex002.c
@@ -0,0 +1,39 @@
+/* Q2) Implemente um programa que, infinitamente, receba, como parâmetro de entrada, um número
+n e retorne os n primeiros números primos existentes depois de n. Por exemplo, se n = 2, a
+resposta será os primos 3 e 5. É necessário salientar que n não precisa ser primo. Seu programa
+para quando n for menor ou igual a zero. */
+
+#include <stdio.h>
+
+int fprimo(int n) {
+
+    if (n < 2) return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
+    }
+
+    return 1;
+}
+
+
+int main(void) {
+
+    while(1) {
+        int n;
+        scanf("%d", &n);
+        if (n <= 0) break;
+        int i = n + 1;
+        int cont = 0;
+        while (cont < n) {
+            if (fprimo(i)) {
+                printf("%d ", i);
+                cont++;
+            }
+            i++;
+        }
+        printf("\n");
+    }
+
+    printf("FIM DE PROGRAMA.");
+    return 0;
+}
