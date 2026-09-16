c898cb191f5e6938109a83c8c544b09e8493a26f
Ayram Veloso <velosoayram@gmail.com>
2026-09-16 08:22:58 -0300

    update

--- prog_c/ex_ro7/listas/ls02/ex003.c
+++ prog_c/ex_ro7/listas/ls02/ex003.c
@@ -12,6 +12,7 @@ int main(void) {
 
         int n, q, b = 0;
 
+        printf("DIGITE UM No: ");
         scanf("%d", &n);
 
         if (n < 0) break;
@@ -33,3 +34,50 @@ int main(void) {
 }
 
 // versão vetor:
+
+#include <stdio.h>
+int main(void) {
+
+    int n;
+
+    while(1) {
+
+        printf("DIGITE UM No: ");
+        scanf("%d", &n);
+
+        if (n < 0) break;
+
+        if (n == 0) {
+            
+            printf("0 | binary: 0");
+            printf("\n\n");
+            continue;
+
+        }
+
+        int x = n;
+        int binario[32];
+        int i = 0;
+
+        while(x > 0) {
+
+            binario[i] = x % 2;
+            x /= 2;
+            i++;
+
+        }
+
+        printf("%d | binary: ", n);
+
+        for (int j = i - 1; j >= 0; j--) {
+
+            printf("%d", binario[j]);
+        }
+
+        printf("\n\n");
+
+    }
+    return 0;
+}
+
+// versão recursiva:
