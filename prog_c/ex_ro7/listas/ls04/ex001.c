c59223120324a650dc7b4c3f7d359d1848d1fd8b
Ayram Veloso <velosoayram@gmail.com>
2026-09-22 21:38:21 -0300

    update

--- prog_c/ex_ro7/listas/ls04/ex001.c
+++ prog_c/ex_ro7/listas/ls04/ex001.c
@@ -4,3 +4,28 @@ letras por predecessoras do alfabeto, trocando maiúsculas por minúsculas. Por 
 shift_troca_string (char *str). A letra ‘a’ deve ser substituída por ‘Z’ (e ‘A’ por ‘z’). Caracteres que não
 forem letras devem ser substituídos por ‘!’. Sabe-se que A = 65, Z = 90, a = 97 e z = 122.*/
 
+#include <stdio.h>
+void shift_troca_string(char *str) {
+
+	int i = 0;
+	while (str[i] != '\0') {
+		if ((str[i] >= 65) && (str[i] <= 90)) {
+			if (str[i] == 65) str[i] = 122; // A -> z
+			else str[i] += 31;
+		} else if ((str[i] >= 97) && (str[i] <= 122)) {
+			if (str[i] == 97) str[i] = 90; // a -> Z
+			else str[i] -= 33;
+		} else str[i] = '!';
+		i++;
+	}
+}
+
+
+int main(void) {
+
+	char str[100];
+	scanf("%s", str); // levei um pau, não sabia que o scanf aqui não necessita de &.
+	shift_troca_string(str);
+	printf("%s", str);		
+	return 0;
+}
